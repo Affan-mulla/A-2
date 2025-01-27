1ea7219db7a909b4b3445c46903ed7377f3b48ba
Affan-mulla <161614493+Affan-mulla@users.noreply.github.com>
2025-01-27 08:17:56 +0530

    Add files via upload

---
+++ Login_Product/Product.js
@@ -0,0 +1,92 @@
+const Product = [
+    {
+        name: "Air Max 90",
+        price: 120,
+        img: "Shoes/Shoe1.png"
+    },
+    {
+        name: "Air Force 1",
+        price: 110,
+        img: "Shoes/shoe2.png"
+    },
+    {
+        name: "Air Zoom Pegasus",
+        price: 130,
+        img: "Shoes/shoe3.png"
+    },
+    {
+        name: "React Infinity Run",
+        price: 160,
+        img: "Shoes/shoe4.png"
+    },
+    {
+        name: "Blazer Mid '77",
+        price: 100,
+        img: "Shoes/shoe5.png"
+    },
+    {
+        name: "Air VaporMax",
+        price: 190,
+        img: "Shoes/shoe6.png"
+    },
+    {
+        name: "Court Vision Low",
+        price: 75,
+        img: "Shoes/shoe7.png"
+    },
+    {
+        name: "ZoomX Vaporfly",
+        price: 250,
+        img: "Shoes/shoe8.png"
+    },
+    
+];
+
+let list = document.querySelector(".list-all");
+
+
+function createProduct(ImgLink,name,price){
+    const product = document.createElement("li")
+    
+    product.setAttribute("class","product-container")
+
+    let img = document.createElement("img")
+    img.setAttribute("src", ImgLink)
+    img.setAttribute("id","product-img")
+
+    let detail = document.createElement("div")
+    detail.setAttribute("class","detail")
+
+    let headDetail = document.createElement("div")
+    
+    let h2 = document.createElement("h2")
+    h2.innerText = name
+    let p = document.createElement("p")
+    p.innerText = "Men's Shoes"
+
+    headDetail.append(h2)
+    headDetail.append(p)
+    
+
+    let priceTag = document.createElement("h2")
+    priceTag.innerText = "$"+price
+
+    let btn = document.createElement("button")
+    btn.textContent = "Buy Now"
+
+    detail.append(headDetail)
+    detail.append(priceTag)
+    detail.append(btn)
+
+    product.append(img)
+    product.append(detail)
+
+    list.append(product)
+
+
+}
+
+for (const element of Product) {
+    createProduct(element.img,element.name,element.price)
+}
+
